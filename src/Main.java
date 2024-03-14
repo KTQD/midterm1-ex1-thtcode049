a9993c6c9e2f7b35f2337a20ab6243972cc07451
thtcode049 <130653444+thtcode049@users.noreply.github.com>
2024-03-14 08:42:57 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("Từng là", "nhạc tình", "MP301", 10000, 240);
+
         mp3.showInfo();
 
-        Book book;
-        book.showInfo();
+        Book book = new Book("Nhà giả kim", "hành trình tìm ra kho báu cuộc đời", "NEU01", 69000, "...", 500, "truyện");
 
+        book.showInfo();
     }
 }
