f399bf81acfd226ae504a482b900948df10cb133
thtcode049 <130653444+thtcode049@users.noreply.github.com>
2024-03-14 08:36:34 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,18 @@
-public class Book {
+public class Book extends Item {
+    public String author;
+    public int numberOfPages;
+    public String genre;
+
+    Book(String name, String description, String ID, int price, String author, int numberOfPages, String genre) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Name: " + name + "\nDescription: " + getDescription() + "\nID: " + ID + "\nPrice: " + price
+            + "\nAuthor: " + author + "\nNumber of pages: " + numberOfPages + "\nGenre: " + genre);
+    }
 }
