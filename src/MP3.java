6b97bc8b8a00c1194a1c9e13c1701223163c7440
thtcode049 <130653444+thtcode049@users.noreply.github.com>
2024-03-14 08:38:43 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,14 @@
-public class MP3 {
+public class MP3 extends Item {
+    public String duration;
+
+
+    MP3(String name, String description, String ID, int price, String duration) {
+        super(name, description, ID, price);
+        this.duration = duration;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Name: " + name + "\nDescription: " + getDescription() + "\nID: " + ID + "\nPrice: " + price + "\nDuration: " + duration);
+    }
 }
