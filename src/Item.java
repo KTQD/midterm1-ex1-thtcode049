244b214edf12d9c7115a41b8b7baf0a485201dda
thtcode049 <130653444+thtcode049@users.noreply.github.com>
2024-03-14 08:37:10 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,22 @@
-public class Item {
+public abstract class Item {
+    public String name;
+    private String description;
+    protected String ID;
+    int price;
+
+    Item(String name, String description, String ID, int price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+
+    public abstract void showInfo();
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
